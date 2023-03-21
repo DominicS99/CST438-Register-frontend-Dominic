60abb135b065b62d4692edb1db8c5536a8b302a9
Dominic <dsagen@csumb.edu>
2023-03-21 15:25:56 -0700

    Re-adding all the features of Create Account

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # CST438 Software Engineering California State University Monterey Bay
 # Student Registration project Front End application
+## Dominic Sagen
 
 This application is written in React and requires node.js to be installed.
 
--- src/components/Semester.js
+++ src/components/Semester.js
@@ -1,35 +1,71 @@
 import React, { Component } from 'react';
+import {ToastContainer, toast } from 'react-toastify';
+import Cookies from 'js-cookie';
+import {SERVER_URL} from '../constants.js'
 import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 import Radio from '@mui/material/Radio';
 import {DataGrid} from '@mui/x-data-grid';
 import {SEMESTER_LIST} from '../constants.js'
+import AddAccount from './AddAccount.js';
+import 'react-toastify/dist/ReactToastify.css';
 
 // user selects from a list of  (year, semester) values
 class Semester extends Component {
     constructor(props) {
       super(props);
-      this.state = {selected: SEMESTER_LIST.length-1 };
+      this.state = {selected: SEMESTER_LIST.length-1, account: {}};
     }
  
-   onRadioClick = (event) => {
+  onRadioClick = (event) => {
     console.log("Semester.onRadioClick "+JSON.stringify(event.target.value));
     this.setState({selected: event.target.value});
   }
+
+  createAccount = (account) => {
+    const tok = Cookies.get('XSRF-TOKEN');
+
+    fetch(`${SERVER_URL}/account`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json',
+                 'X-XSRF-TOKEN': tok  }, 
+      body: JSON.stringify(account)
+    }).then(res => {
+      if (res.ok) {
+        toast.success("Account successfully added", {
+          position: toast.POSITION.BOTTOM_LEFT
+        });
+        this.account = account;
+
+      } else {
+        toast.error("Error when adding", {
+          position: toast.POSITION.BOTTOM_LEFT
+        });
+        console.error('Post http status =' + res.status);
+
+      }
+    }).catch(err => {
+        toast.error("Error when adding", {
+          position: toast.POSITION.BOTTOM_LEFT
+        });
+        console.error(err);
+
+    })
+  }
   
   render() {    
-      const icolumns = [
-      {
+      const icolumns = [{
         field: 'id',
         headerName: 'Year',
         width: 200,
         renderCell: (params) => (
           <div>
             <Radio
-              checked={params.row.id == this.state.selected}
+              checked={params.row.id === this.state.selected}
               onChange={this.onRadioClick}
               value={params.row.id}
               color="default"
@@ -38,9 +74,11 @@ class Semester extends Component {
             { SEMESTER_LIST[params.row.id].year }
           </div>
         )
-      },
-      { field: 'name', headerName: 'Semester', width: 200 }
-      ];       
+      }, { 
+        field: 'name', 
+        headerName: 'Semester', 
+        width: 200 
+      }];       
        
     return (
        <div>
@@ -62,6 +100,10 @@ class Semester extends Component {
                 variant="outlined" color="primary" style={{margin: 10}}>
                 Get Schedule
               </Button>
+              <ButtonGroup>
+                  <AddAccount currAccount={this.createAccount}  />
+				    </ButtonGroup>
+              <ToastContainer autoClose={1500} />   
           </div>
       </div>
     )
